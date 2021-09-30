2221c39bff95c7086fee8aa86fd57e4c7140b166
Roman Yarlykov <yarlykov92@gmail.com>
2021-09-30 22:37:13 +0700

    creating logo component

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
+import Logo from 'Components/Logo';
 import Counter from 'Components/Counter/Counter';
 
 import styles from './homePage.module.scss';
 
 const HomePage = () => (
   <div className={styles.root}>
+    <Logo size={{ width: '106px', height: '40px' }} />
     <Counter />
   </div>
 );
