e79228c151191379156e4a376736aa5d109b811c
Artem Dmitrenko <admitrenko87@gmail.com>
2021-10-03 15:23:11 +0300

    Create Button component

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
 import Counter from 'Components/Counter/Counter';
+import Button from 'Components/Button/Button';
 
 import styles from './homePage.module.scss';
 
 const HomePage = () => (
   <div className={styles.root}>
     <Counter />
+    <Button text="Войти" btnType="withBorder" />
+    <Button text="Зарегистрироваться" btnType="gradient" />
   </div>
 );
 
