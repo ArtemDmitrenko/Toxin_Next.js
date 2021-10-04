3607d66d5d88f3d77252c7b1f1aa82d375e43754
IvanG <ivan.gladyshev.dev@gmail.com>
2021-10-04 17:43:50 +0500

    Manual limit redux-devtools

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,20 @@
-import { createStore } from 'redux';
-import { createWrapper } from 'next-redux-wrapper';
-import { devToolsEnhancer } from 'redux-devtools-extension/developmentOnly';
+import { createStore, Store } from 'redux';
+import { createWrapper, MakeStore } from 'next-redux-wrapper';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from './rootReducer';
 
-const makeStore = () => createStore(rootReducer, devToolsEnhancer({}));
+const environment = process.env.NODE_ENV;
+const isDev = environment === 'development';
+
+let makeStore: MakeStore<Store>;
+
+if (isDev) {
+  makeStore = () => createStore(rootReducer, devToolsEnhancer({}));
+} else {
+  makeStore = () => createStore(rootReducer);
+}
 
 const wrapper = createWrapper(makeStore, { debug: false });
 
