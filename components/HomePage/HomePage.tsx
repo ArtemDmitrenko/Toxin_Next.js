9369ec211eb62e9d29b1d2e0190e19ba659f9f10
Roman Yarlykov <yarlykov92@gmail.com>
2021-10-04 22:25:37 +0700

    creating subscription form

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import Logo from 'Components/Logo/Logo';
 import Counter from 'Components/Counter/Counter';
+import Subscribe from 'Components/Subscribe/Subscribe';
 
 import styles from './homePage.module.scss';
 
@@ -7,6 +8,7 @@ const HomePage = () => (
   <div className={styles.root}>
     <Logo width={106} height={40} alt="Toxin hotel logo" />
     <Counter />
+    <Subscribe action="/" />
   </div>
 );
 
