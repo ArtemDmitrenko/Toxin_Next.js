76dd72d8c9e2cea48ecb422d6c26c324a4bb22fc
Artem Dmitrenko <admitrenko87@gmail.com>
2021-10-06 22:42:51 +0300

    Changed the name of variable

--- components/Reference/Reference.tsx
+++ components/Reference/Reference.tsx
@@ -10,18 +10,18 @@ type ReferenceProps = {
 
 const Reference = (props: ReferenceProps) => {
   const { text, type, size } = props;
-  const classes = [styles.reference];
+  const classesArr = [styles.reference];
 
-  if (type === 'bordered') classes.push(styles.bordered);
-  else if (type === 'solid') classes.push(styles.solid);
-  if (size === 'big') classes.push(styles.big);
-  else if (size === 'small') classes.push(styles.small);
+  if (type === 'bordered') classesArr.push(styles.bordered);
+  else if (type === 'solid') classesArr.push(styles.solid);
+  if (size === 'big') classesArr.push(styles.big);
+  else if (size === 'small') classesArr.push(styles.small);
 
-  const joinedClasses = classes.join(' ');
+  const classes = classesArr.join(' ');
 
   return (
     <Link href="/" passHref>
-      <a href="replace" className={joinedClasses}>
+      <a href="replace" className={classes}>
         {text}
       </a>
     </Link>
