a14b2f4fb3db3999f6111215eec5a5c4d0c3400d
Artem Dmitrenko <admitrenko87@gmail.com>
2021-10-07 00:46:41 +0300

    Refactoring

--- components/BackgroundWithSlogan/BackgroundWithSlogan.tsx
+++ components/BackgroundWithSlogan/BackgroundWithSlogan.tsx
@@ -1,14 +1,11 @@
 import styles from './backgroundWithSlogan.module.scss';
 
-const BackgroundWithSlogan = () => {
-  const sty = 0;
-  return (
-    <div className={styles.main}>
-      <div className={styles.wrapper}>
-        <p className={styles.slogan}>Лучшие номера для вашей работы, отдыха и просто вдохновения</p>
-      </div>
+const BackgroundWithSlogan = () => (
+  <div className={styles.main}>
+    <div className={styles.wrapper}>
+      <p className={styles.slogan}>Лучшие номера для вашей работы, отдыха и просто вдохновения</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default BackgroundWithSlogan;
