e86314e2d083706df72edb4a99155c35cf3154c5
IvanG <ivan.gladyshev.dev@gmail.com>
2021-10-08 13:14:05 +0500

    Develop UI interactive of guests dropdown
    Update Group type
    Add MAX_GROUP_VALUE
    Create function calc group sum
    Create output generator
    Move extend className selectors to arrow functions

--- components/GuestsDropdown/GuestsDropdown.tsx
+++ components/GuestsDropdown/GuestsDropdown.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import num2str from 'Root/utils';
+
 import styles from './guestsDropdown.module.scss';
 
 type GuestsDropdownConfig = Array<{
@@ -14,11 +16,14 @@ type GuestsDropdownProps = {
 
 type Groups = {
   [key: string]: {
-    [key: string]: {
-      title: string,
-      value: number,
-    }
-  }
+    wordforms: [string, string, string],
+    items: {
+      [key: string]: {
+        title: string,
+        value: number,
+      },
+    },
+  },
 };
 
 type GuestsDropdownState = {
@@ -26,6 +31,8 @@ type GuestsDropdownState = {
   output: string,
   groups: Groups,
 };
+
+const MAX_GROUP_VALUE = 10;
 
 const GuestsDropdown = (props: GuestsDropdownProps) => {
   const { list } = props;
@@ -41,16 +48,20 @@ const GuestsDropdown = (props: GuestsDropdownProps) => {
 
     list.forEach((item) => {
       if (prevGroup === item.group) {
-        defaultState.groups[item.group][`item${count}`] = {
+        defaultState.groups[item.group].items[`item${count}`] = {
           title: item.title,
           value: 0,
         };
       } else {
         count = 0;
+
         defaultState.groups[item.group] = {
-          [`item${count}`]: {
-            title: item.title,
-            value: 0,
+          wordforms: item.wordforms,
+          items: {
+            [`item${count}`]: {
+              title: item.title,
+              value: 0,
+            },
           },
         };
       }
@@ -64,6 +75,59 @@ const GuestsDropdown = (props: GuestsDropdownProps) => {
 
   const [dropdown, setDropdown] = useState(defaultState);
 
+  const groupSum = (groupName: string) => {
+    const { items } = dropdown.groups[groupName];
+    let sum = 0;
+
+    Object.values(items).forEach((item) => {
+      sum += item.value;
+    });
+
+    return sum;
+  };
+
+  const outputGenerate = () => {
+    const outputStr: Array<string> = [];
+
+    Object.keys(dropdown.groups).forEach((groupName) => {
+      const groupValue = groupSum(groupName);
+
+      if (groupValue > 0) {
+        outputStr.push(`${groupValue} ${num2str(groupValue, dropdown.groups[groupName].wordforms)}`);
+      }
+    });
+
+    return outputStr.join(', ');
+  };
+
+  const stylesClearButton = () => (
+    `${styles.button} ${styles.buttonClear}`
+  );
+
+  const stylesApplyButton = () => (
+    `${styles.button} ${styles.buttonApply}`
+  );
+
+  const stylesOutput = () => (
+    `${styles.output} ${dropdown.isActive ? styles.outputActive : ''}`
+  );
+
+  const stylesInput = () => (
+    `${styles.input} ${dropdown.isActive ? styles.inputActive : ''}`
+  );
+
+  const stylesMenuWrapper = () => (
+    `${styles.menuWrapper} ${dropdown.isActive ? styles.menuWrapperActive : ''}`
+  );
+
+  const stylesMinus = (item: { value: number, title: string }) => (
+    `${styles.minus} ${Number(item.value) <= 0 ? styles.minusDisabled : ''}`
+  );
+
+  const stylesPlus = (groupName: string) => (
+    `${styles.plus} ${groupSum(groupName) >= MAX_GROUP_VALUE ? styles.plusDisabled : ''}`
+  );
+
   const handleOutputFocus = () => {
     setDropdown((prevState) => ({ ...prevState, isActive: true }));
   };
@@ -73,35 +137,41 @@ const GuestsDropdown = (props: GuestsDropdownProps) => {
   };
 
   const handleMinusClick = (groupName: string, itemName: string) => {
-    setDropdown((prevState) => {
-      if (prevState.groups[groupName][itemName].value < 1) return prevState;
+    if (dropdown.groups[groupName].items[itemName].value <= 0) return;
 
-      return {
-        ...prevState,
-        groups: {
-          ...prevState.groups,
-          [groupName]: {
-            ...prevState.groups[groupName],
-            [itemName]: {
-              ...prevState.groups[groupName][itemName],
-              value: prevState.groups[groupName][itemName].value - 1,
-            },
-          },
-        },
-      };
-    });
-  };
-
-  const handlePlusClick = (groupName: string, itemName: string) => {
     setDropdown((prevState) => ({
       ...prevState,
       groups: {
         ...prevState.groups,
         [groupName]: {
           ...prevState.groups[groupName],
-          [itemName]: {
-            ...prevState.groups[groupName][itemName],
-            value: prevState.groups[groupName][itemName].value + 1,
+          items: {
+            ...prevState.groups[groupName].items,
+            [itemName]: {
+              ...prevState.groups[groupName].items[itemName],
+              value: prevState.groups[groupName].items[itemName].value - 1,
+            },
+          },
+        },
+      },
+    }));
+  };
+
+  const handlePlusClick = (groupName: string, itemName: string) => {
+    if (groupSum(groupName) >= MAX_GROUP_VALUE) return;
+
+    setDropdown((prevState) => ({
+      ...prevState,
+      groups: {
+        ...prevState.groups,
+        [groupName]: {
+          ...prevState.groups[groupName],
+          items: {
+            ...prevState.groups[groupName].items,
+            [itemName]: {
+              ...prevState.groups[groupName].items[itemName],
+              value: prevState.groups[groupName].items[itemName].value + 1,
+            },
           },
         },
       },
@@ -113,7 +183,7 @@ const GuestsDropdown = (props: GuestsDropdownProps) => {
       const newState = { ...prevState };
 
       Object.values(newState.groups).forEach((group) => {
-        Object.values(group).forEach((item) => {
+        Object.values(group.items).forEach((item) => {
           Object.defineProperty(item, 'value', { value: 0 });
         });
       });
@@ -124,30 +194,48 @@ const GuestsDropdown = (props: GuestsDropdownProps) => {
 
   return (
     <div className={styles.guestsDropdown} onFocus={handleOutputFocus} onBlur={handleOutputBlur}>
-      <div className={`${styles.output} ${dropdown.isActive ? styles.outputActive : ''}`}>
-        <input type="text" className={`${styles.input} ${dropdown.isActive ? styles.inputActive : ''}`} placeholder="Сколько гостей" readOnly value={dropdown.output} />
+      <div className={stylesOutput()}>
+        <input
+          type="text"
+          className={stylesInput()}
+          placeholder="Сколько гостей"
+          readOnly
+          value={outputGenerate()}
+        />
       </div>
-      <div className={`${styles.menuWrapper} ${dropdown.isActive ? styles.menuWrapperActive : ''}`}>
+      <div className={stylesMenuWrapper()}>
         <div tabIndex={0} role="menu" className={styles.menu}>
           <ul className={styles.list}>
             {Object.entries(dropdown.groups).map(([groupName, group]) => (
-              Object.entries(group).map(([itemName, item]) => (
+              Object.entries(group.items).map(([itemName, item]) => (
                 <li key={item.title} className={styles.item}>
                   <div>{item.title}</div>
                   <div className={styles.control}>
-                    <button type="button" className={`${styles.minus} ${Number(item.value) < 1 ? styles.minusDisabled : ''}`} onClick={handleMinusClick.bind(null, groupName, itemName)}> - </button>
+                    <button
+                      type="button"
+                      className={stylesMinus(item)}
+                      onClick={handleMinusClick.bind(null, groupName, itemName)}
+                    >
+                      -
+                    </button>
                     <div className={styles.counter}>
                       {item.value}
                     </div>
-                    <button type="button" className={styles.plus} onClick={handlePlusClick.bind(null, groupName, itemName)}> + </button>
+                    <button
+                      type="button"
+                      className={stylesPlus(groupName)}
+                      onClick={handlePlusClick.bind(null, groupName, itemName)}
+                    >
+                      +
+                    </button>
                   </div>
                 </li>
               ))
             ))}
           </ul>
           <div className={`${styles.buttons} ${styles.buttonsNonEmpty}`}>
-            <button type="button" className={`${styles.button} ${styles.buttonClear}`} onClick={handleClearClick}>Очистить</button>
-            <button type="button" className={`${styles.button} ${styles.buttonApply}`} onClick={handleOutputBlur}>Применить</button>
+            <button type="button" className={stylesClearButton()} onClick={handleClearClick}>Очистить</button>
+            <button type="button" className={stylesApplyButton()} onClick={handleOutputBlur}>Применить</button>
           </div>
         </div>
       </div>
