1740af2ffaffe10a4a12d2852e137df87ebc2868
Artem Dmitrenko <admitrenko87@gmail.com>
2021-10-09 10:44:25 +0300

    Resolve merge conflict

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import Counter from 'Components/Counter/Counter';
+import Subscribe from 'Components/Subscribe/Subscribe';
 import Reference from 'Components/Reference/Reference';
 
 import styles from './homePage.module.scss';
@@ -6,6 +7,7 @@ import styles from './homePage.module.scss';
 const HomePage = () => (
   <div className={styles.root}>
     <Counter />
+    <Subscribe action="/" />
     <Reference text="Зарегистрироваться" type="solid" size="small" />
     <Reference text="Зарегистрироваться" type="solid" size="big" />
     <Reference text="click me" type="bordered" size="small" />
