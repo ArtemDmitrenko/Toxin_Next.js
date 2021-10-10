91491ea29582689d972aeee5e337e35e6ed5bfd3
w.zdorowa <w.zdorowa@gmail.com>
2021-10-10 16:16:54 +0300

    creating header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,60 @@
+import Link from 'next/link';
+import Logo from 'Components/Logo/Logo';
+import Reference from 'Components/Reference/Reference';
+
 import styles from './header.module.scss';
 
-type HeaderProps = { content: string };
+type HeaderProps = {
+  menu: Array<{
+    id: number;
+    name: string,
+    href: string,
+    subMenu: Array<{
+      id: number;
+      name: string,
+      href: string
+    }>,
+  }>,
+};
 
 const Header = (props: HeaderProps) => {
-  const { content } = props;
-
+  const { menu } = props;
   return (
-    <h1 className={styles.root}>{content}</h1>
+    <div className={styles.content}>
+      <Logo width={110} height={40} alt="Logo" />
+      <ul className={styles.header}>
+        {menu.map((item) => (item.subMenu.length > 0 ? (
+          <li className={styles.titleSubMenu} key={item.id}>
+            <Link href={item.href}>
+              <a className={styles.link} href={item.href}>
+                {item.name}
+                <i className={styles.arrow} />
+              </a>
+            </Link>
+            <ul className={styles.subMenu}>
+              {item.subMenu.map((element) => (
+                <li className={styles.subMenuItem} key={element.id}>
+                  <Link href={element.href}>
+                    <a className={styles.link} href={element.href}>{element.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        )
+          : (
+            <li className={styles.listItem} key={item.id}>
+              <Link href={item.href}>
+                <a className={styles.link} href={item.href}>{item.name}</a>
+              </Link>
+            </li>
+          )))}
+      </ul>
+      <div className={styles.buttons}>
+        <Reference text="Войти" type="bordered" size="small" />
+        <Reference text="Зарегистрироваться" type="solid" size="small" />
+      </div>
+    </div>
   );
 };
 
--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -2,19 +2,63 @@ import Logo from 'Components/Logo/Logo';
 import Counter from 'Components/Counter/Counter';
 import Subscribe from 'Components/Subscribe/Subscribe';
 import Reference from 'Components/Reference/Reference';
+import Header from 'Components/Header/Header';
 
 import styles from './homePage.module.scss';
 
-const HomePage = () => (
-  <div className={styles.root}>
-    <Logo width={106} height={40} alt="Toxin hotel logo" />
-    <Counter />
-    <Subscribe action="/" />
-    <Reference text="Зарегистрироваться" type="solid" size="small" />
-    <Reference text="Зарегистрироваться" type="solid" size="big" />
-    <Reference text="click me" type="bordered" size="small" />
-    <Reference text="click me" type="bordered" size="big" />
-  </div>
-);
+const HomePage = () => {
+  const navigation = [
+    {
+      id: 1,
+      name: 'О нас',
+      href: '/',
+      subMenu: [],
+    },
+    {
+      id: 2,
+      name: 'Услуги',
+      href: '/',
+      subMenu: [
+        { id: 1, name: 'Аренда конференц-зала', href: '/' },
+        { id: 2, name: 'Трансфер до отеля', href: '/' },
+        { id: 3, name: 'Аренда транспорта', href: '/' },
+        { id: 4, name: 'Камера хранения / сейф', href: '/' },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Вакансии',
+      href: '/',
+      subMenu: [],
+    },
+    {
+      id: 4,
+      name: 'Новости',
+      href: '/',
+      subMenu: [],
+    },
+    {
+      id: 5,
+      name: 'Соглашения',
+      href: '/',
+      subMenu: [
+        { id: 1, name: 'Обработка персональных данных', href: '/' },
+        { id: 2, name: 'Условия бронирования', href: '/' },
+      ],
+    },
+  ];
+  return (
+    <div className={styles.root}>
+      <Logo width={106} height={40} alt="Toxin hotel logo" />
+      <Counter />
+      <Subscribe action="/" />
+      <Reference text="Зарегистрироваться" type="solid" size="small" />
+      <Reference text="Зарегистрироваться" type="solid" size="big" />
+      <Reference text="click me" type="bordered" size="small" />
+      <Reference text="click me" type="bordered" size="big" />
+      <Header menu={navigation} />
+    </div>
+  );
+};
 
 export default HomePage;
