42d73de0208a3e268744727e07cfe0fd55cd4639
Roman Yarlykov <yarlykov92@gmail.com>
2021-10-11 19:06:57 +0700

    coponent Subscribe removed from the current branch

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
 import Logo from 'Components/Logo/Logo';
 import Counter from 'Components/Counter/Counter';
-import Subscribe from 'Components/Subscribe/Subscribe';
 import DateRange from 'Components/DateRange/DateRange';
 
 import styles from './homePage.module.scss';
@@ -9,7 +8,6 @@ const HomePage = () => (
   <div className={styles.root}>
     <Logo width={106} height={40} alt="Toxin hotel logo" />
     <Counter />
-    <Subscribe />
     <DateRange headers={['прибытие', 'выезд']} placeholder="ДД.ММ.ГГГГ" />
   </div>
 );
--- redux/subscribe/subscribeActions.ts
+++
@@ -1,8 +0,0 @@
-import SubscribeActionTypes from './subscribeActionTypes';
-
-const subscribe = (email: string) => (<const>{
-  type: SubscribeActionTypes.SUBSCRIBE,
-  email,
-});
-
-export default subscribe;
