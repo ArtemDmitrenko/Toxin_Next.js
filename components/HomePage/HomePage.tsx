409887e6e6d1fac3105c9f32e6fcdd37f03af8e3
w.zdorowa <w.zdorowa@gmail.com>
2021-10-13 16:39:09 +0300

    deleted the result of the conflict

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import DateRange from 'Components/DateRange/DateRange';
 import CopyrightBar from 'Components/CopyrightBar/CopyrightBar';
 import Subscribe from 'Components/Subscribe/Subscribe';
 import Reference from 'Components/Reference/Reference';
-import FooterMobile from '../FooterMobile/FooterMobile';
+import FooterMobile from 'Components/FooterMobile/FooterMobile';
 import FooterDesktop from 'Components/FooterDesktop/FooterDesktop';
 import footerItems from 'Components/FooterDesktop/footer-items.json';
 
@@ -36,7 +36,6 @@ const HomePage = () => (
     <Reference text="Зарегистрироваться" type="solid" size="big" />
     <Reference text="click me" type="bordered" size="small" />
     <Reference text="click me" type="bordered" size="big" />
-<<<<<<< components/HomePage/HomePage.tsx
     <Reference text="Перейти к оплате" type="directed" size="big" />
     <CopyrightBar text="Copyright © 2018 Toxin отель. Все права защищены." forMobile={false} />
     <FooterMobile />
