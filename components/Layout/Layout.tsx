5148da46746fc4939c52a57d52700a7d6f16cc86
Artem Dmitrenko <admitrenko87@gmail.com>
2021-10-18 17:02:57 +0300

    Removed from Layout navigationItems to Public

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,6 +4,7 @@ import Header from 'Components/Header/Header';
 import FooterDesktop from 'Components/FooterDesktop/FooterDesktop';
 import footerItems from 'Components/FooterDesktop/footer-items.json';
 import FooterMobile from 'Components/FooterMobile/FooterMobile';
+import navigation from 'Root/public/navigation.json';
 
 const addNewEmail = (email: string) => {
   const subscriptionData = {
@@ -18,43 +19,6 @@ type LayoutProps = {
 
 const Layout = (props: LayoutProps) => {
   const { children, title } = props;
-  const navigation = [
-    {
-      id: 1,
-      name: 'О нас',
-      href: '/',
-    },
-    {
-      id: 2,
-      name: 'Услуги',
-      href: '/',
-      subMenu: [
-        { id: 1, name: 'Аренда конференц-зала', href: '/' },
-        { id: 2, name: 'Трансфер до отеля', href: '/' },
-        { id: 3, name: 'Аренда транспорта', href: '/' },
-        { id: 4, name: 'Камера хранения / сейф', href: '/' },
-      ],
-    },
-    {
-      id: 3,
-      name: 'Вакансии',
-      href: '/',
-    },
-    {
-      id: 4,
-      name: 'Новости',
-      href: '/',
-    },
-    {
-      id: 5,
-      name: 'Соглашения',
-      href: '/',
-      subMenu: [
-        { id: 1, name: 'Обработка персональных данных', href: '/' },
-        { id: 2, name: 'Условия бронирования', href: '/' },
-      ],
-    },
-  ];
 
   return (
     <div>
