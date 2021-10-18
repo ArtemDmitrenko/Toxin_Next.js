599d560fc9122bc1c92f0dc48e0177fbfac55820
IvanG <ivan.gladyshev.dev@gmail.com>
2021-10-18 13:34:51 +0500

    Remove SearchRoomsPage component

--- pages/search-rooms.tsx
+++ pages/search-rooms.tsx
@@ -1,5 +1,57 @@
-import SearchRoomsPage from 'Components/SearchRoomsPage/SearchRoomsPage';
+import Dropdown, { DropdownConfig } from 'Components/Dropdown/Dropdown';
 
-const SearchRooms = () => <SearchRoomsPage />;
+import styles from './search-rooms.module.scss';
+
+const guestsDropdownConfig: DropdownConfig = [
+  {
+    title: 'Взрослые',
+    group: 'guests',
+    wordforms: ['гость', 'гостя', 'гостей'],
+  },
+  {
+    title: 'Дети',
+    group: 'guests',
+    wordforms: ['гость', 'гостя', 'гостей'],
+  },
+  {
+    title: 'Младенцы',
+    group: 'babies',
+    wordforms: ['младенец', 'младенца', 'младенцев'],
+  },
+];
+
+const facilitiesDropdownConfig: DropdownConfig = [
+  {
+    title: 'Спальни',
+    group: 'bedrooms',
+    defaultValue: 2,
+    wordforms: ['спальня', 'спальни', 'спален'],
+  },
+  {
+    title: 'Кровати',
+    group: 'beds',
+    defaultValue: 1,
+    wordforms: ['кровать', 'кровати', 'кроватей'],
+  },
+  {
+    title: 'Ванные комнаты',
+    group: 'bathrooms',
+    defaultValue: 1,
+    wordforms: ['ванная комната', 'ванные комнаты', 'ванных комнат'],
+  },
+];
+
+const SearchRooms = () => (
+  <div>
+    <div>
+      <Dropdown list={guestsDropdownConfig} placeholder="Сколько гостей" />
+      <Dropdown list={facilitiesDropdownConfig} placeholder="Выберите удобства" isButtons={false} />
+    </div>
+    <div className={styles.row}>
+      <Dropdown list={guestsDropdownConfig} placeholder="Сколько гостей" />
+      <Dropdown list={facilitiesDropdownConfig} placeholder="Выберите удобства" isButtons={false} />
+    </div>
+  </div>
+);
 
 export default SearchRooms;
