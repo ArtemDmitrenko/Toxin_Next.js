0485762e578c21eed6a73aa33ddf8c42ce61bdbb
IvanG <ivan.gladyshev.dev@gmail.com>
2021-10-18 13:45:49 +0500

    Remove NotFoundPage component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,20 @@
-import NotFoundPage from 'Components/NotFoundPage/NotFoundPage';
+import Link from 'next/link';
 
-const Custom404 = () => <NotFoundPage />;
+import styles from './404.module.scss';
+
+const Custom404 = () => (
+  <div className={styles.wrapper}>
+    <div className={styles.card}>
+      <h1>Ой! Данная страница не найдена :( </h1>
+      <h2>
+        <Link href="/">
+          <a className={styles.link} href="replace" tabIndex={0}>
+            Вернуться на главную
+          </a>
+        </Link>
+      </h2>
+    </div>
+  </div>
+);
 
 export default Custom404;
