268e96cb3678eee9988fc7b555ad00f1550a7947
Roman Yarlykov <yarlykov92@gmail.com>
2021-10-23 20:54:35 +0700

    making room details page base markup

--- pages/rooms/[room].tsx
+++ pages/rooms/[room].tsx
@@ -1,5 +1,15 @@
-import mockData from 'Root/public/room-mock/room.json';
+import Layout from 'Components/Layout/Layout';
 import Collage from 'Components/Collage/Collage';
+import Comment from 'Components/Comment/Comment';
+import RulesList from 'Components/RulesList/RulesList';
+import RoomInformation from 'Components/RoomInformation/RoomInformation';
+
+import mockData from 'Root/public/room-mock/room.json';
+import userComment from 'Components/Comment/comment.json';
+import rulesList from 'Components/RulesList/rulesList.json';
+import roomInformation from 'Components/RoomInformation/roomInformation.json';
+
+import styles from './room.module.scss';
 
 type RoomProps = {
   data: {
@@ -19,9 +29,55 @@ const Room = (props: RoomProps) => {
   const { data } = props;
 
   return (
-    <div>
+    <Layout title={`Room ${data.room}`}>
       <Collage images={data.images} />
-    </div>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.information}>
+            <RoomInformation heading="Сведения о номере" info={roomInformation} />
+            <section className={styles.chart}>
+              <h2 className={styles.header}>Впечатления от номера</h2>
+              <div className={styles.mockChart} />
+            </section>
+            <section className={styles.feedback}>
+              <h2 className={styles.header}>Отзывы посетителей номера</h2>
+              <Comment
+                srcIcon={userComment.srcIcon}
+                userName={userComment.userName}
+                date={new Date(userComment.date)}
+                text={userComment.text}
+                like={userComment.like}
+                onChange={() => {}}
+              />
+              <Comment
+                srcIcon={userComment.srcIcon}
+                userName={userComment.userName}
+                date={new Date(userComment.date)}
+                text={userComment.text}
+                like={userComment.like}
+                onChange={() => {}}
+              />
+            </section>
+            <RulesList
+              rulesHeader="Правила"
+              rulesList={rulesList}
+            />
+            <div className={styles.cancel}>
+              <h2 className={styles.header}>Отмена</h2>
+              <p className={styles.cancelText}>
+                Бесплатная отмена в течение 48 ч.
+                После&nbsp;этого при отмене не позднее
+                чем&nbsp;за 5 дн. до прибытия вы получите
+                полный возврат за вычетом сбора за услуги.
+              </p>
+            </div>
+          </div>
+          <div className={styles.bookingCard}>
+            <div className={styles.bookingMockCard} />
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
