40eece6d2c7b02a758b03ece1275097b8bf8682d
Artem Dmitrenko <admitrenko87@gmail.com>
2021-10-26 10:16:34 +0300

    Add non-portable space in cost

--- components/ReservationCard/helpers/formatCost.tsx
+++ components/ReservationCard/helpers/formatCost.tsx
@@ -1,5 +1,5 @@
 const formatCost = (cost: number): string => (
-  cost.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')
+  cost.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1\u00A0')
 );
 
 export default formatCost;
