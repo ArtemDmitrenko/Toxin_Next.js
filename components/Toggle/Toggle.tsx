162ba07d33aa9a686c51701766a5f03ef48560fb
w.zdorowa <w.zdorowa@gmail.com>
2021-10-27 10:57:42 +0300

    Create Toggle component

---
+++ components/Toggle/Toggle.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+import styles from './toggle.module.scss';
+
+type ToggleData = {
+  name: string,
+  isChecked: boolean,
+};
+
+type ToggleProps = {
+  title: string,
+  isChecked?: boolean,
+  name: string,
+  onChange?: (data: ToggleData) => void,
+};
+
+const Toggle = (props: ToggleProps) => {
+  const {
+    title,
+    isChecked = false,
+    name,
+    onChange,
+  } = props;
+
+  const [checked, setChecked] = useState(isChecked);
+
+  const handleToggleChange = () => {
+    setChecked(!checked);
+
+    if (onChange) {
+      onChange({ name, isChecked: !checked });
+    }
+  };
+
+  return (
+    <label className={styles.toggle}>
+      <input className={styles.content} name={name} type="checkbox" checked={checked} onChange={handleToggleChange} />
+      <span className={styles.indicator} />
+      <span className={styles.title}>{title}</span>
+    </label>
+  );
+};
+
+export type { ToggleProps, ToggleData };
+export default Toggle;
