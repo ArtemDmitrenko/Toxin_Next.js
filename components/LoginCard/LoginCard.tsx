e3193517edeb0cafb7861d4069e1084b9167b6d2
Roman Yarlykov <yarlykov92@gmail.com>
2021-10-28 21:13:52 +0700

    login card refactoring

--- components/LoginCard/LoginCard.tsx
+++ components/LoginCard/LoginCard.tsx
@@ -1,82 +1,88 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Field, Form } from 'react-final-form';
 
+import hasValidateEmail from 'Root/utils/hasValidateEmail';
 import Reference from 'Components/Reference/Reference';
 
 import styles from './loginCard.module.scss';
 
-type FormValues = {
-  email: string,
+type FormProps = {
+  onSubmit: ({ loginEmail, password }: { loginEmail: string, password: string }) => void,
+};
+
+type LoginFormData = {
+  loginEmail: string,
   password: string,
 };
 
-const LoginCard = () => {
-  const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
-  const handleFormSubmit = async (values: FormValues) => {
-    await sleep(300);
-    console.log(JSON.stringify(values, undefined, 2));
+const LoginCard = ({ onSubmit }: FormProps) => {
+  const requiredEmail = (email: string) => {
+    if (!email) return 'Введите email';
+    if (!hasValidateEmail(email)) return 'Введите корректный email';
+    return '';
   };
 
-  const requiredEmail = (value: string) => (value ? undefined : 'Введите email');
-  const requiredPassword = (value: string) => (value ? undefined : 'Введите пароль');
+  const requiredPassword = (password: string) => (password ? undefined : 'Введите пароль');
+
+  const buttonClass = `${styles.button} ${styles.directed} ${styles.big}`;
 
   return (
-    <div className={styles.mockWrapper}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Войти</h1>
-        <Form onSubmit={handleFormSubmit}>
-          {({ handleSubmit, submitting }) => (
-            <form onSubmit={handleSubmit}>
-              <Field
-                name="email"
-                placeholder="Email"
-                validate={requiredEmail}
-              >
-                {({ input, meta, placeholder }) => (
-                  <div className={styles.email}>
-                    {meta.error && meta.touched && <span>{meta.error}</span>}
-                    <input
-                      {...input}
-                      className={styles.field}
-                      placeholder={placeholder}
-                    />
-                  </div>
-                )}
-              </Field>
-              <Field
-                name="password"
-                type="password"
-                placeholder="Пароль"
-                validate={requiredPassword}
-              >
-                {({ input, meta, placeholder }) => (
-                  <div className={styles.password}>
-                    {meta.error && meta.touched && <span>{meta.error}</span>}
-                    <input
-                      {...input}
-                      className={styles.field}
-                      placeholder={placeholder}
-                    />
-                  </div>
-                )}
-              </Field>
-              <button
-                className={styles.button}
-                type="submit"
-                disabled={submitting}
-              >
-                войти
-              </button>
-            </form>
-          )}
-        </Form>
-        <div className={styles.question}>
-          <span>Нет аккаунта на Toxin?</span>
-          <Reference href="/404" text="создать" type="bordered" size="big" />
-        </div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Войти</h1>
+      <Form onSubmit={onSubmit}>
+        {({ handleSubmit, submitting }) => (
+          <form onSubmit={handleSubmit}>
+            <Field
+              name="loginEmail"
+              type="email"
+              placeholder="Email"
+              validate={requiredEmail}
+            >
+              {({ input, meta, placeholder }) => (
+                <div className={styles.email}>
+                  {meta.error && meta.touched && <span>{meta.error}</span>}
+                  <input
+                    {...input}
+                    className={styles.field}
+                    placeholder={placeholder}
+                  />
+                </div>
+              )}
+            </Field>
+            <Field
+              name="password"
+              type="password"
+              placeholder="Пароль"
+              validate={requiredPassword}
+            >
+              {({ input, meta, placeholder }) => (
+                <div className={styles.password}>
+                  {meta.error && meta.touched && <span>{meta.error}</span>}
+                  <input
+                    {...input}
+                    className={styles.field}
+                    placeholder={placeholder}
+                  />
+                </div>
+              )}
+            </Field>
+            <button
+              className={buttonClass}
+              type="submit"
+              disabled={submitting}
+            >
+              войти
+            </button>
+          </form>
+        )}
+      </Form>
+      <div className={styles.question}>
+        <span>Нет аккаунта на Toxin?</span>
+        <Reference href="/auth/sign-up" text="создать" type="bordered" size="big" />
       </div>
     </div>
   );
 };
 
+export type { LoginFormData };
 export default LoginCard;
--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -1,9 +1,12 @@
 import Layout from 'Components/Layout/Layout';
 import LoginCard from 'Components/LoginCard/LoginCard';
+import BackgroundAuth from 'Components/BackgroundAuth/BackgroundAuth';
 
 const SignIn = () => (
-  <Layout title="Sign up">
-    <LoginCard />
+  <Layout title="Sign in">
+    <BackgroundAuth>
+      <LoginCard onSubmit={() => {}} />
+    </BackgroundAuth>
   </Layout>
 );
 
