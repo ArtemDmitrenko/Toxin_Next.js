57c9d0ad34921bc19c630eb089a3f153daaffc0e
IvanG <ivan.gladyshev.dev@gmail.com>
2021-10-28 12:47:31 +0500

    Much simplify component
    RadioGroup change to RadioButton
    Make component non-controllable

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -1,27 +1,27 @@
 import Layout from 'Components/Layout/Layout';
-import RadioGroup from 'Components/RadioGroup/RadioGroup';
+import RadioButton from 'Components/RadioButton/RadioButton';
 
 import styles from './sign-up.module.scss';
 
-const radioList = [
-  {
-    value: 'male',
-    content: 'Мужчина',
-  },
-  {
-    value: 'female',
-    content: 'Женщина',
-  },
-];
-
 const SignUp = () => (
   <Layout title="Sign up">
-    <RadioGroup
-      list={radioList}
-      name="sex"
-      className={styles.radioGroup}
-      radioClassName={styles.radioButton}
-    />
+    <div className={styles.radioGroup}>
+      <div className={styles.radioButton}>
+        <RadioButton
+          name="sex"
+          value="male"
+          content="Мужчина"
+          isDefaultChecked
+        />
+      </div>
+      <div className={styles.radioButton}>
+        <RadioButton
+          name="sex"
+          value="female"
+          content="Женщина"
+        />
+      </div>
+    </div>
   </Layout>
 );
 
