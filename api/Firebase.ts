39e94304147b9afb15277e42626eb09644bc18b9
w.zdorowa <w.zdorowa@gmail.com>
2021-11-03 17:01:26 +0300

    Fix the logic of interaction with firebase

--- api/Firebase.ts
+++ api/Firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 import firebaseCfg from './firebaseConfig';
 
@@ -6,6 +7,8 @@ abstract class Firebase {
   private static firebaseConfig = firebaseCfg;
 
   public static firebase = initializeApp(this.firebaseConfig);
+
+  public static auth = getAuth;
 }
 
 export default Firebase;
--- pages/auth/password-recovery.tsx
+++ pages/auth/password-recovery.tsx
@@ -1,22 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import Firebase from 'api/Firebase';
 
 import Layout from 'Components/Layout/Layout';
 import BackgroundAuth from 'Components/BackgroundAuth/BackgroundAuth';
 import PasswordRecoveryCard from 'Components/PasswordRecoveryCard/PasswordRecoveryCard';
 
 const PasswordRecovery = () => {
-  initializeApp({
-    apiKey: 'AIzaSyDjlISC_n81sYbH9ziz66rrYcOnEkNIWQo',
-    authDomain: 'toxin-coolhackers.firebaseapp.com',
-    projectId: 'toxin-coolhackers',
-    storageBucket: 'toxin-coolhackers.appspot.com',
-    messagingSenderId: '297163595644',
-    appId: '1:297163595644:web:8cc3be821b5e331c2a0ef9',
-    measurementId: 'G-71SVNT6E1K',
-  });
-
-  const auth = getAuth();
+  const auth = Firebase.auth();
   auth.languageCode = 'ru';
 
   const onSubmit = async (email: string) => {
