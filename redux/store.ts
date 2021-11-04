b11b7de9f7761a2fdfc2ef970acf53860465da0e
Roman Yarlykov <yarlykov92@gmail.com>
2021-11-04 13:55:46 +0700

    fixed: bug with creating double store

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,6 @@
 import { createStore, Store } from 'redux';
 import { createWrapper, MakeStore } from 'next-redux-wrapper';
 import { devToolsEnhancer } from 'redux-devtools-extension';
-import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from './rootReducer';
 
@@ -9,21 +8,21 @@ const environment = process.env.NODE_ENV;
 const isDev = environment === 'development';
 
 let makeStore: MakeStore<Store>;
+let store: Store;
 
 if (isDev) {
-  makeStore = () => createStore(rootReducer, devToolsEnhancer({}));
+  makeStore = () => {
+    store = createStore(rootReducer, devToolsEnhancer({}));
+    return store;
+  };
 } else {
   makeStore = () => createStore(rootReducer);
 }
 
 const wrapper = createWrapper(makeStore, { debug: false });
 
-const store = configureStore({
-  reducer: rootReducer,
-});
-
 type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
 
 export type { RootState, AppDispatch };
-export { wrapper, store };
+export { wrapper };
