d1b1b57415dabb7637e980d99adb49dc79b7f1ac
Roman Yarlykov <yarlykov92@gmail.com>
2021-11-17 20:06:06 +0700

    added react-final-form

--- components/ReviewCard/ReviewCard.tsx
+++ components/ReviewCard/ReviewCard.tsx
@@ -1,37 +1,50 @@
-import Reference from 'Components/Reference/Reference';
 import { useState } from 'react';
-import CircularProgressBar from '../CircularProgressBar/CircularProgressBar';
+import { Field, Form } from 'react-final-form';
+
+import { useAppSelector } from 'Root/redux/hooks';
+import Reference from 'Components/Reference/Reference';
+import CircularProgressBar from 'Components/CircularProgressBar/CircularProgressBar';
 
 import styles from './reviewCard.module.scss';
 
 type ReviewCardProps = {
   maxLength: number,
+  onSubmit: (data: { userId: string, comment: string }) => void;
 };
 
-const ReviewCard = ({ maxLength }: ReviewCardProps) => {
+const ReviewCard = ({ maxLength, onSubmit }: ReviewCardProps) => {
   const [textLength, setTextLength] = useState(0);
   const [isOpen, setOpen] = useState(false);
+  const { userId } = useAppSelector((state) => state.auth);
+
+  const letterCounter = (text: string) => (
+    text ? setTextLength(text.length) : setTextLength(0)
+  );
+
+  const handleFormSubmit = ({ text }:{ text: string }) => {
+    onSubmit({
+      userId,
+      comment: text,
+    });
+  };
 
   const handleTitleButtonClick = () => { setOpen(!isOpen); };
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    const { length } = e.target.value;
 
-    setTextLength(length);
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const { target } = event;
 
-    // Reset field height
-    e.target.style.height = 'inherit';
+    if (target instanceof HTMLTextAreaElement) {
+      target.style.height = 'inherit';
+      const computed = window.getComputedStyle(target);
 
-    // Get the computed styles for the element
-    const computed = window.getComputedStyle(e.target);
+      const height = parseInt(computed.getPropertyValue('border-top-width'), 5)
+                   + parseInt(computed.getPropertyValue('padding-top'), 5)
+                   + target.scrollHeight
+                   + parseInt(computed.getPropertyValue('padding-bottom'), 5)
+                   + parseInt(computed.getPropertyValue('border-bottom-width'), 5);
 
-    // Calculate the height
-    const height = parseInt(computed.getPropertyValue('border-top-width'), 5)
-                 + parseInt(computed.getPropertyValue('padding-top'), 5)
-                 + e.target.scrollHeight
-                 + parseInt(computed.getPropertyValue('padding-bottom'), 5)
-                 + parseInt(computed.getPropertyValue('border-bottom-width'), 5);
-
-    e.target.style.height = `${height}px`;
+      target.style.height = `${height}px`;
+    }
   };
 
   return (
@@ -44,23 +57,37 @@ const ReviewCard = ({ maxLength }: ReviewCardProps) => {
         Оставить отзыв
       </button>
       {isOpen && (
-      <form>
-        <textarea
-          className={styles.field}
-          maxLength={500}
-          required
-          onKeyDown={handleKeyDown}
-        />
-        <div className={styles.footer}>
-          <Reference isButton buttonType="submit" text="отправить отзыв" type="bordered" size="big" />
-          <CircularProgressBar
-            sqSize={44}
-            textLength={textLength}
-            maxLength={maxLength}
-            strokeWidth={4}
-          />
-        </div>
-      </form>
+      <Form onSubmit={handleFormSubmit}>
+        {({ handleSubmit, submitting }) => (
+          <form onSubmit={handleSubmit}>
+            <Field
+              className={styles.field}
+              name="text"
+              component="textarea"
+              maxLength={500}
+              required
+              validate={letterCounter}
+              onKeyDown={handleKeyDown}
+            />
+            <div className={styles.footer}>
+              <Reference
+                isButton
+                buttonType="submit"
+                text="отправить отзыв"
+                type="bordered"
+                size="big"
+                disabled={submitting}
+              />
+              <CircularProgressBar
+                sqSize={44}
+                textLength={textLength}
+                maxLength={maxLength}
+                strokeWidth={4}
+              />
+            </div>
+          </form>
+        )}
+      </Form>
       )}
     </div>
   );
